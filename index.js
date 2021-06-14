cc8af1de751a7cb967d45f402f30dacf978bb22c
danmada <danmada@gmail.com>
2021-06-13 22:24:36 -0500

    commit tree 67906c578c0307ca6426bf579858aac874afb974

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+
+
+
+const returnFirstTwoDrivers = function(array) {
+    
+    return array.slice(0,2)
+}
+
+
+const returnLastTwoDrivers = function(array) {
+    
+    return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+selectingDrivers[0]()
+
+function createFareMultiplier(multiNum) {
+
+    return function multiplier(fare) {
+        return multiNum * fare}
+}
+
+function fareDoubler(fare) {
+    let timesTwo = fare * 2
+    return timesTwo
+}
+
+function fareTripler(fare) {
+    let timesThree = fare * 3
+    return timesThree
+}
+
+function selectDifferentDrivers(array, returnFunc) {
+
+    if (returnFunc === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(array)
+    } else {
+        return returnLastTwoDrivers(array)
+    }
+
+}
